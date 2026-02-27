d4bfe0211a787fa3676fe9a04f0375f086efa854
kotendro <i.abramtsov@gmail.com>
2026-02-28 01:31:29 +0500

    feat(client): start point

--- client/js/app.js
+++ client/js/app.js
@@ -1,24 +1,59 @@
 import { getDetail } from "/js/service/api.js"
 import { initCardForm } from "/js/components/card_form/card_form.controller.js"
+import { initCardDetail } from "/js/components/card_detail/card_detail.contoller.js"
+import { createStore } from "/js/store.js"
 
 const cardFormDialog = document.querySelector("#cardFormDialog")
+const cardDetailDialog = document.querySelector("#cardDetailDialog")
 const cardForm = document.querySelector("#cardForm")
 const addCardBtn = document.querySelector("#addCardBtn")
-const editCardBtn = document.querySelector("#editCardBtn")
+const detailCardBtn = document.querySelector("#detailCardBtn")
+
+const store = createStore({
+    screen: "list",
+    currentCard: null,
+    formMode: "create"
+})
 
 const cardFormApi = initCardForm({
     dialog: cardFormDialog,
     form: cardForm,
+    store: store
 })
 
+const cardDetailApi =  initCardDetail({
+    dialog: cardDetailDialog,
+    store: store
+})
+
+store.subscribe((state) => {
+    if (state.screen === "form") {
+        cardDetailApi.close()
+        cardFormApi.open()
+        cardFormApi.render(state)
+        return
+    }
+
+    if (state.screen === "detail") {
+        cardFormApi.close()
+        cardDetailApi.open()
+        cardDetailApi.render(state)
+        return
+    }
+
+    cardFormApi.close()
+    cardDetailApi.close()
+})
+
+
 const card = await getDetail("f8642195-8d20-4a2a-a6e2-8f437641891b")
+store.setState({currentCard: card})
 console.log(card)
 
 addCardBtn.addEventListener("click", () => {
-    cardFormApi.openCreate()
+    store.setState({ screen:"form", formMode:"create" })
 })
 
-editCardBtn.addEventListener("click", () => {
-    cardFormApi.openEdit(card)
+detailCardBtn.addEventListener("click", () => {
+    store.setState({ screen:"detail" })
 })
-
